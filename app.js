037fa4347be40a8e040b89a5ee60ff6173382ee4
arbmos <oscarecho@fastmail.fm>
2015-04-20 17:58:28 -0600

    add code to make api call and retrieve feeds data and display the data in feeds div

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	$('.login').submit(function(e){
 		e.preventDefault();
-		var SERVER_LOGIN_URL = "http://informerly.com/api/v1/users/sign_in";
+		var SERVER_BASE_URL = "http://informerly.com/api/v1/";
+		var SERVER_LOGIN_URL = SERVER_BASE_URL + "users/sign_in";
+		var SERVER_ARTICLE_URL = SERVER_BASE_URL + "feeds";
 		var user = $('#email').val();
 		var pass = $('#password').val();
 		
@@ -17,6 +19,28 @@ $(document).ready(function() {
 			success: function(data) {
 				if (data.success) {
 					$('p').text("You're Logged In");
+					console.log(data);
+					
+					var auth_token = data.auth_token;
+					console.log(auth_token);
+					
+					$.ajax({
+						type: "GET",
+						url: SERVER_ARTICLE_URL,
+						dataType: 'json',
+						data: {
+							"auth_token": auth_token,
+							"client_id": "alk2jdlkjxx4",
+							content: true
+						},
+						success: function(data) {
+							console.log(data.links);
+							for (var i = 0; i < 20; i++) {
+								$(".feeds").prepend("<a href='" + data.links[i].url +"'><p>" + data.links[i].title +"</p></a>");
+								
+							}
+						}
+					});
 				}
 			}
 		});
