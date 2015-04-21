898737f7f507462bf5f1298078d1dc07fa5608e7
arbmos <oscarecho@fastmail.fm>
2015-04-21 12:56:08 -0600

    add api call to display bookmarks feeds

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
 	var SERVER_BASE_URL = "http://informerly.com/api/v1/";
 	var SERVER_LOGIN_URL = SERVER_BASE_URL + "users/sign_in";
 	var SERVER_ARTICLE_URL = SERVER_BASE_URL + "feeds";
+	var SERVER_BOOKMARKS_URL = SERVER_BASE_URL + "bookmarks";
 	
 	chrome.storage.local.get("auth_token", function(data) {
 		auth_token = data.auth_token;
 		if (auth_token) {
 			$('p').text("You're Logged In");
 			showFeeds();
+			showBookmarks();
 			$('.login').hide();
 		}
 	});
@@ -38,6 +40,7 @@ $(document).ready(function() {
 					});
 					
 					showFeeds();
+					showBookmarks();
 				}
 			}
 		});
@@ -55,7 +58,29 @@ $(document).ready(function() {
 			},
 			success: function(data) {
 				for (var i = 0; i < data.count; i++) {
-					$(".feeds").prepend("<a target='_blank' href='" + data.links[i].url +"'><p>" + data.links[i].title +"</a><br><small>" + data.links[i].source +" | " + data.links[i].reading_time +" min read</small><br></p>");					
+					$(".feeds").append("<a target='_blank' href='" + data.links[i].url +"'><p>" + data.links[i].title +"</a><br><small>" + data.links[i].source +" | " + data.links[i].reading_time +" min read</small><br></p>");					
+				}
+			}
+		});
+	}
+	
+	function showBookmarks() {
+		$.ajax({
+			type: "GET",
+			url: SERVER_BOOKMARKS_URL,
+			dataType: 'json',
+			data: {
+				"auth_token": auth_token,
+				"client_id": "alk2jdlkjxx4"
+			},
+			success: function(data) {
+				console.log(data);
+				if (data.count == 0) {
+					$(".bookmarks").append("<p>You do not currently have any bookmarks.</p>");
+				} else {
+					for (var i = 0; i < data.count; i++) {
+						$(".bookmarks").append("<a target='_blank' href='" + data.links[i].url +"'><p>" + data.links[i].title +"</a><br><small>" + data.links[i].source +" | " + data.links[i].reading_time +" min read</small><br></p>");
+					}
 				}
 			}
 		});
